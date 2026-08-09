646fe5e887296891f874de8af02c4514a6cae363
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 08:29:30 -0400

    refactor: enhance Button component to accept props for expanded state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Time from "./components/Time";
 import Location from "./components/Location";
 import Quote from "./components/Quote";
 
 export default function App() {
+  const [isDetailsVisible, setIsDetailsVisible] = useState(false);
   return (
     <>
       <Quote></Quote>
       <Time></Time>
       <Location></Location>
-      <Button></Button>
+      <Button
+        isExpanded={isDetailsVisible}
+        setIsExpanded={() => setIsDetailsVisible((prev) => !prev)}
+      ></Button>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-
 import styles from "./Button.module.css";
 import MoreIcon from "../assets/icons/icon-arrow-down.svg";
 import LessIcon from "../assets/icons/icon-arrow-up.svg";
 
-export default function Button() {
-  const [isExpanded, setIsExpanded] = useState(false);
+export default function Button({ isExpanded, setIsExpanded }) {
   return (
-    <button
-      className={styles.btn}
-      type="button"
-      onClick={() => setIsExpanded((prev) => !prev)}
-    >
+    <button className={styles.btn} type="button" onClick={setIsExpanded}>
       {isExpanded ? (
         <>
           <span>Less</span>
