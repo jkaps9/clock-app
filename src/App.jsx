65ecd50c3e77db2e9c5b107807ad618f2550d7f4
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-08 21:17:06 -0400

    handle the button fully within the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import Button from "./components/Button";
-import MoreIcon from "./assets/icons/icon-arrow-down.svg";
-import LessIcon from "./assets/icons/icon-arrow-up.svg";
+
 export default function App() {
   return (
     <>
-      <Button>
-        {"More"}{" "}
-        <div className="icon-circle">
-          <img src={MoreIcon} alt="" aria-hidden="true" />
-        </div>
-      </Button>
-      <Button>
-        {"Less"}{" "}
-        <div className="icon-circle">
-          <img src={LessIcon} alt="" aria-hidden="true" />
-        </div>
-      </Button>
+      <Button></Button>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,32 @@
-import styles from "./Button.module.css";
+import { useState } from "react";
 
-export default function Button({ type, onClick = null, children }) {
+import styles from "./Button.module.css";
+import MoreIcon from "../assets/icons/icon-arrow-down.svg";
+import LessIcon from "../assets/icons/icon-arrow-up.svg";
+
+export default function Button() {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <button
       className={styles.btn}
-      type={type}
-      onClick={onClick ? onClick : null}
+      type="button"
+      onClick={() => setIsExpanded((prev) => !prev)}
     >
-      {children}
+      {isExpanded ? (
+        <>
+          <span>Less</span>
+          <div className={styles.iconCircle}>
+            <img src={LessIcon} alt="" aria-hidden="true" />
+          </div>
+        </>
+      ) : (
+        <>
+          <span>More</span>
+          <div className={styles.iconCircle}>
+            <img src={MoreIcon} alt="" aria-hidden="true" />
+          </div>
+        </>
+      )}
     </button>
   );
 }
