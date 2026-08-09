6fc773a5aa0fdecf704283b1b6bba5262db294bd
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 08:40:15 -0400

    feat: add Greeting component to display time-based messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Time from "./components/Time";
 import Location from "./components/Location";
 import Quote from "./components/Quote";
 import Details from "./components/Details";
+import Greeting from "./components/Greeting";
 
 export default function App() {
   const [isDetailsVisible, setIsDetailsVisible] = useState(false);
   return (
     <>
       {!isDetailsVisible && <Quote></Quote>}
+      <Greeting timeHour={11}></Greeting>
       <Time></Time>
       <Location></Location>
       <Button
