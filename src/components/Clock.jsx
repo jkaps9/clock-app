c20cff55caa504de75ef950e8d66e663ec94eeb0
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-08 21:40:55 -0400

    get time info

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -30,14 +30,26 @@ export default function Clock() {
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h3>Your IP-Based Time Info</h3>
       <p>
-        <strong>Current Time:</strong>{" "}
+        <strong>Current Time:</strong>
         {new Date(timeData.datetime).toLocaleString()}
       </p>
       <p>
         <strong>Timezone:</strong> {timeData.timezone}
       </p>
       <p>
-        <strong>UTC Offset:</strong> {timeData.utc_offset}
+        <strong>Unix Time:</strong> {timeData.unixtime}
+      </p>
+      <p>
+        <strong>abbreviation:</strong> {timeData.abbreviation}
+      </p>
+      <p>
+        <strong>DST:</strong> {timeData.dst ? "true" : "false"}
+      </p>
+      <p>
+        <strong>DST Offset:</strong> {timeData.dst_offset}
+      </p>
+      <p>
+        <strong>IP:</strong> {timeData.client_ip}
       </p>
     </div>
   );
