d00702930fc64ae47f98be39e4b60818e4896256
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 08:33:42 -0400

    refactor: conditionally render Quote and Details components based on visibility state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Button from "./components/Button";
 import Time from "./components/Time";
 import Location from "./components/Location";
 import Quote from "./components/Quote";
+import Details from "./components/Details";
 
 export default function App() {
   const [isDetailsVisible, setIsDetailsVisible] = useState(false);
   return (
     <>
-      <Quote></Quote>
+      {!isDetailsVisible && <Quote></Quote>}
       <Time></Time>
       <Location></Location>
       <Button
         isExpanded={isDetailsVisible}
         setIsExpanded={() => setIsDetailsVisible((prev) => !prev)}
       ></Button>
+      {!isDetailsVisible && <Details></Details>}
     </>
   );
 }
---
+++ src/components/Details.jsx
@@ -0,0 +1,22 @@
+export default function Details() {
+  return (
+    <ul>
+      <li>
+        <p className="label">Current timezone</p>
+        <p className="value">Europe/London</p>
+      </li>
+      <li>
+        <p className="label">Day of the year</p>
+        <p className="value">295</p>
+      </li>
+      <li>
+        <p className="label">Day of the week</p>
+        <p className="value">5</p>
+      </li>
+      <li>
+        <p className="label">Week number</p>
+        <p className="value">42</p>
+      </li>
+    </ul>
+  );
+}
