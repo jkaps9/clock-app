d4340d93a1a9a1393b74ca955ac99e1443c4b52a
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 09:20:11 -0400

    style: add locationText class to Location component for improved styling

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,12 +1,12 @@
+import styles from "./Location.module.css";
 export default function Location({ locationData, error, loading }) {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <>
-      <p>
-        In
-        {locationData.cityName}, {locationData.regionName}
+      <p className={styles.locationText}>
+        In {locationData.cityName}, {locationData.regionName}
       </p>
     </>
   );
