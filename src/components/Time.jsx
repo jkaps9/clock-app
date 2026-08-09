004ea6b5b89392e6bca180fecd8a3417273162c5
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 09:23:59 -0400

    style: add timeText and timeZoneText classes to Clock component for improved styling

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,18 +1,19 @@
+import styles from "./Time.module.css";
+
 export default function Clock({ timeData, error, loading }) {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div>
-      <p>
+      <p className={styles.timeText}>
         {new Date(timeData.datetime).toLocaleTimeString([], {
           hour12: false,
           hour: "2-digit",
           minute: "2-digit",
-        })}{" "}
-        {timeData.abbreviation}
+        })}
+        <span className={styles.timeZoneText}>{timeData.abbreviation}</span>
       </p>
-      <p></p>
     </div>
   );
 }
