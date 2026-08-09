bbea177f7e1aa132d0acf53ec85d85b240900fce
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 08:52:05 -0400

    refactor: lift location state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useTimeUrl } from "./hooks/useTimeUrl";
+import { useLocationUrl } from "./hooks/useLocationUrl";
 
 import Button from "./components/Button";
 import Time from "./components/Time";
@@ -11,13 +12,22 @@ import Greeting from "./components/Greeting";
 export default function App() {
   const [isDetailsVisible, setIsDetailsVisible] = useState(false);
   const { timeData, error, loading } = useTimeUrl();
+  const {
+    locationData,
+    error: locationError,
+    loading: locationLoading,
+  } = useLocationUrl();
 
   return (
     <>
       {!isDetailsVisible && <Quote></Quote>}
       <Greeting timeHour={11}></Greeting>
       <Time timeData={timeData} error={error} loading={loading}></Time>
-      <Location></Location>
+      <Location
+        locationData={locationData}
+        error={locationError}
+        loading={locationLoading}
+      ></Location>
       <Button
         isExpanded={isDetailsVisible}
         setIsExpanded={() => setIsDetailsVisible((prev) => !prev)}
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,14 +1,11 @@
-import { useLocationUrl } from "../hooks/useLocationUrl";
-
-export default function Location() {
-  const { locationData, error, loading } = useLocationUrl();
-
+export default function Location({ locationData, error, loading }) {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <>
       <p>
+        In
         {locationData.cityName}, {locationData.regionName}
       </p>
     </>
