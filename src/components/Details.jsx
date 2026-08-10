3ebccbfa4010b978383413c4ef5fba1cc2854301
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-09 20:57:47 -0400

    style: enhance layout and styling in Details component for improved structure and readability

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -14,6 +14,9 @@ export default function Details({ timeData }) {
           <p className={styles.detailsItemLabel}>Day of the year</p>
           <p className={styles.detailsItemValue}>{timeData.day_of_year}</p>
         </li>
+      </ul>
+      <div className={styles.divider}></div>
+      <ul className={styles.detailsList}>
         <li className={styles.detailsListItem}>
           <p className={styles.detailsItemLabel}>Day of the week</p>
           <p className={styles.detailsItemValue}>{timeData.day_of_week + 1}</p>
